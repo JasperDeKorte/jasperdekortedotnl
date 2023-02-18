694e63bc5c18e15db933a53213b791400f54c502
JasperDeKorte <jasperDKcode@gmail.com>
2023-02-18 11:36:01 +0100

    remove second text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <header className="App-header">
         <div className="container">
           <h1>U KNO THA WAE TO MAH WEBSITE</h1>
-          <h1>U KNO THA WAE TO MAH WEBSITE</h1>
         </div>
       </header>
     </div>
