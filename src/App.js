5895aa219b6fc179d26399ceb390d40b8e5fc6a8
JasperDeKorte <jasperDKcode@gmail.com>
2023-02-18 10:57:52 +0100

    U know da wae

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="container">
-          <h1>MIJN KAOLO WEBSITE</h1>
+          <h1>U KNO THA WAE TO MAH WEBSITE</h1>
         </div>
       </header>
     </div>
